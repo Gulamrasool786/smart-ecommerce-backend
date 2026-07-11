59b1886cf98c9f42dedc001c381e716a5ff9a343
Gulam Rasool <nadeemraza227756@gmail.com>
2026-07-11 10:41:53 +0530

    Fix JWT secret env name

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,12 +1,15 @@
 import jwt from "jsonwebtoken";
 
-const generateToken = (userId) =>{
-    return jwt.sign(
-        {id: userId},
-        process.env.jwt_SECRET,
-        {
-            expiresIn: "7d",
-        }
-    );
+const generateToken = (userId) => {
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is missing in environment variables");
+  }
+
+  return jwt.sign(
+    { id: userId },
+    process.env.JWT_SECRET,
+    { expiresIn: "7d" }
+  );
 };
+
 export default generateToken;
